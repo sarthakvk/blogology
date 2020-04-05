149f198ab38dd4a44fde3e34966acdffbade108b
Restyled.io <commits@restyled.io>
2020-04-05 04:00:31 +0000

    Restyled by reorder-python-imports

--- migrations/env.py
+++ migrations/env.py
@@ -1,13 +1,12 @@
 from __future__ import with_statement
 
-from flask import current_app
 import logging
 from logging.config import fileConfig
 
+from alembic import context
+from flask import current_app
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
-from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
